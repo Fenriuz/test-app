cda68ceaf73412850cd0ce71ecc882f8c9aa6d54
Cesar Leonardo <cesarleonardo537@gmail.com>
2021-09-05 23:23:12 -0600

    Show/Hide a Component in Tab Two, done

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,15 +1,34 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function TabTwoScreen() {
+  const [readMore, setReadMore] = React.useState(true);
+
+  const description = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+ aliquid hic pariatur eligendi dolorem illo corporis, laboriosam unde
+ fugiat. Fugiat autem illum  et doloribus, rem sint iste quisquam iure
+ consectetur? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+ Corporis, odio ab maiores quae asperiores ut eum natus illo, incidunt
+ temporibus recusandae eius accusantium. Culpa voluptas tenetur omnis
+ molestias perspiciatis laborum! Lorem ipsum dolor sit amet consectetur
+ adipisicing elit. Voluptatibus aliquid hic pariatur eligendi dolorem
+ illo corporis, laboriosam unde fugiat. Fugiat autem illum et doloribus,
+ rem sint iste quisquam iure consectetur? Lorem ipsum dolor sit amet
+ consectetur adipisicing elit. Corporis, odio ab maiores quae asperiores
+ ut eum natus illo, incidunt temporibus recusandae eius accusantium.
+ Culpa voluptas tenetur omnis molestias perspiciatis laborum!`;
+
+  const descriptionSubString = description.substring(0, 35);
+
+  const handlerClickText = () => setReadMore(!readMore);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text>{readMore ? `${descriptionSubString}...` : description}</Text>
+
+      <Text style={{ color: "blue" }} onPress={handlerClickText}>
+        {readMore ? `read more` : `read less`}
+      </Text>
     </View>
   );
 }
@@ -17,16 +36,7 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
